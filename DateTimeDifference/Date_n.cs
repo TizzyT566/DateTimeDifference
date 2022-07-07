0b6588a624051e72550d9b216a97d334b5235e79
Thien Huynh <tizzyt566@gmail.com>
2022-07-07 18:07:42 -0400

    Add project files.

---
+++ DateTimeDifference/Date_n.cs
@@ -0,0 +1,65 @@
+﻿namespace DateTimeDifference;
+
+public class Date_n
+{
+    private static readonly int[] daysPerMonth = new int[] { 31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    private readonly int _day, _month, _year;
+
+    private bool IsLeapYear() => _year % 4 == 0 && _year % 100 != 0;
+
+    private int DaysLeftInYear()
+    {
+        int total = 0;
+
+        for (int i = _month; i < 12; i++)
+            total += daysPerMonth[i];
+
+        if (_month < 2)
+        {
+            total += daysPerMonth[_month - 1] - _day + 1;
+            total += IsLeapYear() ? 29 : 28;
+        }
+        else if (_month == 2)
+        {
+            total += IsLeapYear() ? 29 - _day + 1 : 28 - _day + 1;
+        }
+        else
+        {
+            total += daysPerMonth[_month - 1] - _day + 1;
+        }
+
+        return total;
+    }
+
+    private int DaysInYear()
+    {
+        int total = _day - 1;
+
+        for (int i = 0; i < _month - 1; i++)
+            total += daysPerMonth[i];
+
+        if (_month >= 2)
+            total += IsLeapYear() ? 29 : 28;
+
+        return total;
+    }
+
+    public Date_n(int day, int month, int year)
+    {
+        _day = day;
+        _month = month;
+        _year = year;
+    }
+
+    public static int GetDifference(Date_n d1, Date_n d2)
+    {
+        int total = d1.DaysLeftInYear();
+
+        for (int i = d1._year + 1; i < d2._year; i++)
+            total += new Date_n(1, 1, i).DaysLeftInYear();
+
+        total += d2.DaysInYear();
+
+        return total;
+    }
+}
---
+++ DateTimeDifference/Program.cs
@@ -0,0 +1,8 @@
+﻿using DateTimeDifference;
+
+Date_n d1 = new(8, 8, 2008);
+Date_n d2 = new(16, 3, 2099);
+
+int difference = Date_n.GetDifference(d1, d2);
+
+Console.WriteLine(difference);
